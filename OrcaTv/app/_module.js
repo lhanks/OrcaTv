4ba056dac300d2150bb9f885d15ed1fd8492bc0e
Leslie Hanks <leslie.hanks@sciencetrax.com>
2017-09-14 18:23:32 -0500

    Added feed and displayed it on the home page

---
+++ OrcaTv/app/_module.js
@@ -0,0 +1,30 @@
+﻿angular.module('orca', [
+	'ngResource'
+]);
+
+angular.module('orca')
+	.factory('Feeds', function ($q, $http, $resource) {
+		return {
+			get: function () {
+				return $http({ method: 'GET', url: '/app/feeds.xml' });
+			},
+			get2: function () {
+				var resource = $resource('http://www.orcatv.net/reader/trial1/php/get-feed-content.php?feedurl=http://www.npr.org/rss/rss.php?id=1001&_=1505429345075');
+				return resource.get();
+			}
+		};
+	})
+
+	.controller('home', function ($scope, Feeds) {
+		Feeds.get().then(function (result) {
+//			$scope.test = result.data;
+			$scope.items = xmlToJSON.parseString(result.data).rss[0].channel[0].item;
+			debugger;
+		});
+
+//		$scope.test = Feeds.get();
+//		Feeds.get().then(function(result) {
+//			debugger;
+//		});
+	});
+
